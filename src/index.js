0b099ee2a54002120a53be4930a3fe00c8d52f63
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-03-23 19:00:07 +0100

    fix?

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <App />
